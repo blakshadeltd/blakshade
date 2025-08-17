b231c83a91138541785dca21ae6531676a862897
blakshadeltd <salesbabyclu@gmail.com>
2025-08-17 11:50:21 +0600

    update1

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        hover: 'var(--hover)'
+      },
+    },
+  },
+  plugins: [],
+}
