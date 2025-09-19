f2ff0fb295eda0f5c0f584201b7f4e24b7bdc01c
blakshadeltd <salesbabyclu@gmail.com>
2025-09-19 13:55:47 +0600

    09.19.2025

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import HeaderMobileNav from "./component/MobileNav";
 import DesktopNav from "./component/DesktopNav";
 import StickyNav from "./component/StickyNav";
 import Footer from "./component/footer";
-import localFont from 'next/font/local';
+import localFont from "next/font/local";
 import { ReduxProvider } from "@/store/provider";
 import PersistGateWrapper from "@/store/PersistGateWrapper";
 import CallToAction from "./component/CallToAction";
+import Script from "next/script";
 
-const MillingDuplex1mm = localFont({ src: './MillingDuplex1mm.woff2' });
+const MillingDuplex1mm = localFont({ src: "./MillingDuplex1mm.woff2" });
 
 export default function RootLayout({
   children,
@@ -19,20 +20,32 @@ export default function RootLayout({
   return (
     <html lang="en" className={MillingDuplex1mm.className}>
       <body>
-        <SpeedInsights/>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WYKXYZPBF4"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WYKXYZPBF4');
+          `}
+        </Script>
+
+        <SpeedInsights />
         <ReduxProvider>
           <PersistGateWrapper>
             <DesktopNav />
             <StickyNav />
             <HeaderMobileNav />
-            <main className="bg-[var(--background)] mt-4">
-              {children}
-            </main>
-            <CallToAction/>
+            <main className="bg-[var(--background)] mt-4">{children}</main>
+            <CallToAction />
             <Footer />
           </PersistGateWrapper>
         </ReduxProvider>
-        <SpeedInsights/>
+        <SpeedInsights />
       </body>
     </html>
   );
