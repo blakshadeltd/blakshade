fddfd1d8863fc353a376aef7517fe3ae8ae2158a
blakshadeltd <salesbabyclu@gmail.com>
2025-09-22 19:55:34 +0600

    09.22.2025

--- app/component/SSRNav.tsx
+++ app/component/SSRNav.tsx
@@ -1,32 +1,27 @@
 import Link from "next/link";
-import { menuData } from "./menuData";
-import { infoMenuData } from "./infoMenuData";
 
 export default function SSRNav() {
   return (
     <nav className="sr-only" aria-label="Site Navigation (SEO)">
       <ul>
-        {/* Generator Categories from menuData */}
-        {Object.values(menuData.Generators).flat().map((item) => (
-          <li key={item.href}>
-            <Link href={item.href}>{item.name}</Link>
-          </li>
-        ))}
+        {/* Main Pages */}
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/generators/cummins">Cummins Generators</Link></li>
+        <li><Link href="/generators/cat">CAT Generators</Link></li>
+        <li><Link href="/generators/silent">Silent Generators</Link></li>
+        <li><Link href="/generators/open">Open Generators</Link></li>
+                <li><Link href="/generators/50Hz">50Hz Generators</Link></li>
+        <li><Link href="/generators/60Hz">60Hz Generators</Link></li>
 
         {/* Info Pages */}
-        {infoMenuData.map((item) => (
-          <li key={item.href}>
-            <Link href={item.href}>{item.title}</Link>
-          </li>
-        ))}
+        <li><Link href="/info/about-us">About Us</Link></li>
+        <li><Link href="/info/glossary">Power Generation Glossary</Link></li>
+        <li><Link href="/info/payment-and-shipment">Payment and Shipment</Link></li>
+        <li><Link href="/info/worldwide-voltage-frequency">Voltages and Frequencies</Link></li>
 
         {/* Utility Pages */}
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link href="/build-genset">Build Genset</Link>
-        </li>
+        <li><Link href="/build-genset">Build Genset</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
