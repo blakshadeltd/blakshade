c9c3cfb5e7a1d8921915dc06afc51f7a43f25679
blakshadeltd <salesbabyclu@gmail.com>
2025-09-23 18:26:50 +0600

    09.23.2025

--- next.config.ts
+++ next.config.ts
@@ -5,5 +5,13 @@ const nextConfig = {
   },
   // Your other settings here
 };
+module.exports = {
+  images: {
+    loader: "custom",
+    loaderFile: "./cloudflare-loader.js",
+    domains: ["blakshade.com"], // adjust if you use www
+  },
+};
+
 
 module.exports = nextConfig;
